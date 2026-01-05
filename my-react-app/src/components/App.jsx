97c6be54e9e41ab9dc12481579d32fcdac7a7d97
miraclelarry2002 <miraclelarry02@gmail.com>
2026-01-05 19:40:21 +0100

    made a little change

--- my-react-app/src/components/App.jsx
+++ my-react-app/src/components/App.jsx
@@ -6,20 +6,20 @@ import List from "./lists";
 import Axios from 'axios';
 import "./App.css"
 
-const SERVER_URL = import.meta.env.SERVER_URL || "http://localhost:3000"
+const SERVER_URL = import.meta.env.SERVER_URL
 function App(){
 
     const [keeperarray, setkeeperarray] = useState([])
 
     function add(item){
         //// //////////////////////////// to pass data to the server from the front end ////////////////////////////////
-        Axios.post(`${SERVER_URL}/additem`, item).then((res)=>console.log(res.data))
+        Axios.post(`${VITE_SERVER_URL}/additem`, item).then((res)=>console.log(res.data))
     }
 
 
     ////////////////////// use of useEffect;
     useEffect(()=>{
-        Axios.get(`${SERVER_URL}/getitem`).then((res)=>{
+        Axios.get(`${VITE_SERVER_URL}/getitem`).then((res)=>{
             setkeeperarray(res.data.data)
         })
     },[keeperarray])
@@ -27,7 +27,7 @@ function App(){
     function deleteItem(id){
         console.log(id)
     ////////////////////////////// sending the index of the number to be deleted from the database
-        Axios.post(`${SERVER_URL}/delete`, {index:id}).then((res)=>{
+        Axios.post(`${VITE_SERVER_URL}/delete`, {index:id}).then((res)=>{
             setkeeperarray(res.data.users)
         })
     }
